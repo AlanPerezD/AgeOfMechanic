e8e30558cdcbe761dcddbb663ed115a3db04f137
AlanPerezDaniel <danielmorocheap@gmail.com>
2022-02-15 13:18:21 -0300

    Agregado items y Renombre de un itemGroup a neolithic

--- src/main/java/net/fabricmc/ageofmechanic/item/ModItemGroup.java
+++ src/main/java/net/fabricmc/ageofmechanic/item/ModItemGroup.java
@@ -7,7 +7,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 
 public class ModItemGroup {
-    public static final ItemGroup FLINT_PICKAXE = FabricItemGroupBuilder.build(new Identifier(AgeOfMechanic.MOD_ID,
-            "flint"), () -> new ItemStack(ModItems.FLINT_PICKAXE));
+    public static final ItemGroup NEOLITHIC = FabricItemGroupBuilder.build(new Identifier(AgeOfMechanic.MOD_ID,
+            "neolithic"), () -> new ItemStack(ModItems.FLINT_PICKAXE));
     }
 
--- src/main/java/net/fabricmc/ageofmechanic/item/ModItems.java
+++ src/main/java/net/fabricmc/ageofmechanic/item/ModItems.java
@@ -6,11 +6,21 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import net.fabricmc.ageofmechanic.item.ModItemGroup;
 
 public class ModItems{
 
     public static final Item FLINT_PICKAXE = registerItem("flint_pickaxe",
-            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+            new Item(new FabricItemSettings().group(ModItemGroup.NEOLITHIC)));
+
+    public static final Item FLINT_AXE = registerItem("flint_axe",
+            new Item(new FabricItemSettings().group(ModItemGroup.NEOLITHIC)));
+
+    public static final Item FLINT_SHOVEL = registerItem("flint_shovel",
+            new Item(new FabricItemSettings().group(ModItemGroup.NEOLITHIC)));
+
+    public static final Item FLINT_HOE = registerItem("flint_hoe",
+            new Item(new FabricItemSettings().group(ModItemGroup.NEOLITHIC)));
 
     public static Item registerItem(String name, Item item){
         return net.minecraft.util.registry.Registry.register(Registry.ITEM, new Identifier(AgeOfMechanic.MOD_ID, name), item);
