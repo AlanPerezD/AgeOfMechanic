2db2e2c948cadf8cc4d4bfb07a71ea1ee8932305
AlanPerez-dev <danielmorocheap@gmail.com>
2022-02-20 10:18:50 -0300

    test

--- src/main/java/net/fabricmc/ageofmechanic/block/entity/DemoBlockEntity.java
+++ src/main/java/net/fabricmc/ageofmechanic/block/entity/DemoBlockEntity.java
@@ -1,27 +1,38 @@
 package net.fabricmc.ageofmechanic.block.entity;
 
+import net.fabricmc.ageofmechanic.item.inventory.ImplementedInventory;
 import net.fabricmc.ageofmechanic.registry.AgeOfMechanicEntities;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.inventory.Inventories;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.util.math.BlockPos;
 
-public class DemoBlockEntity extends BlockEntity {
-    // porque usaban NBT para guadar valores (asi funcionaban los cofres)
-    private int number = 7;
+public class DemoBlockEntity extends BlockEntity implements ImplementedInventory {
+    private final DefaultedList<ItemStack> items =
+            DefaultedList.ofSize(2,ItemStack.EMPTY);
 
     public DemoBlockEntity(BlockPos pos, BlockState state){
         super (AgeOfMechanicEntities.DEMO_BLOCK_ENTITY, pos ,state);
     }
+
     @Override
-    public void writeNbt(NbtCompound tag) {
-        tag.putInt("number", number);
-        super.writeNbt(tag);
+    public DefaultedList<ItemStack> getItems() {
+        return items;
     }
+
     @Override
     public void readNbt(NbtCompound tag) {
         super.readNbt(tag);
-        number = tag.getInt("number");
+        Inventories.readNbt(tag, items);
+    }
+
+    @Override
+    public void writeNbt(NbtCompound tag) {
+        Inventories.writeNbt(tag, items);
+        super.writeNbt(tag);
     }
 
 }
--- src/main/java/net/fabricmc/ageofmechanic/registry/AgeOfMechanicEntities.java
+++ src/main/java/net/fabricmc/ageofmechanic/registry/AgeOfMechanicEntities.java
@@ -5,16 +5,16 @@ import net.fabricmc.ageofmechanic.block.entity.DemoBlockEntity;
 import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.util.registry.Registry;
+
 import static net.fabricmc.ageofmechanic.block.ModBlocks.DEMO_BLOCK;
 
 public class AgeOfMechanicEntities {
 
-    public static BlockEntityType<DemoBlockEntity> DEMO_BLOCK_ENTITY = Registry.register(Registry.BLOCK_ENTITY_TYPE, "ageofmechanic:demo_block_entity", FabricBlockEntityTypeBuilder.create(DemoBlockEntity::new, DEMO_BLOCK).build(null));
+    public static BlockEntityType<DemoBlockEntity> DEMO_BLOCK_ENTITY = FabricBlockEntityTypeBuilder.create(DemoBlockEntity::new, DEMO_BLOCK).build(null);
 
 
     public static void registerBlockEntities(){
-        AgeOfMechanic.LOGGER.info("Registering BlockEntities for" + AgeOfMechanic.MOD_ID);
-
+        Registry.register(Registry.BLOCK_ENTITY_TYPE, AgeOfMechanic.MOD_ID + ":clay_urn", DEMO_BLOCK_ENTITY);
     }
 
 }
